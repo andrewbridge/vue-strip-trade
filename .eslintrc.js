de80f43cff9a1eca3a74259abb9ea44c2da7b31b
Andrew Bridge <me@andrewhbridge.co.uk>
2019-01-14 23:30:05 +0000

    Ensure helpers actually import

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,6 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'vue/no-use-v-if-with-v-for': 'off', // Necessary for legs
-    'import/named': 'off', // lint doesn't seem to like functions exported by object
   },
   parserOptions: {
     parser: 'babel-eslint',
--- src/store/helpers.js
+++ src/store/helpers.js
@@ -2,29 +2,35 @@ import { random } from 'lodash';
 import moment from 'moment';
 
 const dateFormat = 'YYYY-MM-DD';
+const getTradeObject = state => id => state.trades.find(trade => trade.id === id);
+const getTradeIndex = state => id => state.trades.findIndex(trade => trade.id === id);
+const getTotalsReducer = (totals, option) => ({
+  low: totals.low + option.lowValue,
+  high: totals.high + option.highValue,
+});
 
-export default {
-  getTradeObject: state => id => state.trades.find(trade => trade.id === id),
-  getTradeIndex: state => id => state.trades.findIndex(trade => trade.id === id),
-  getTotalsReducer(totals, option) {
-    return { low: totals.low + option.lowValue, high: totals.high + option.highValue };
-  },
-  generateLegs(baseOption) {
-    const beginDate = moment(baseOption.beginDate);
-    const legs = [];
-    for (let i = 0; i < baseOption.expiries; i += 1) {
-      legs.push({
-        optionClass: baseOption.optionClass,
-        type: baseOption.type === 'put' ? 'call' : 'put',
-        beginDate: beginDate.format(dateFormat),
-        endDate: beginDate.clone().add(1, 'month').subtract(1, 'day').format(dateFormat),
-        notionalInAmount: baseOption.notionalInAmount,
-        notionalInType: baseOption.notionalInType === 'sell' ? 'buy' : 'sell',
-        lowValue: random(-1000, 500, false),
-        highValue: random(-500, 1000, false),
-      });
-      beginDate.add(1, 'month');
-    }
-    return legs;
-  },
+const generateLegs = (baseOption) => {
+  const beginDate = moment(baseOption.beginDate);
+  const legs = [];
+  for (let i = 0; i < baseOption.expiries; i += 1) {
+    legs.push({
+      optionClass: baseOption.optionClass,
+      type: baseOption.type === 'put' ? 'call' : 'put',
+      beginDate: beginDate.format(dateFormat),
+      endDate: beginDate.clone().add(1, 'month').subtract(1, 'day').format(dateFormat),
+      notionalInAmount: baseOption.notionalInAmount,
+      notionalInType: baseOption.notionalInType === 'sell' ? 'buy' : 'sell',
+      lowValue: random(-1000, 500, false),
+      highValue: random(-500, 1000, false),
+    });
+    beginDate.add(1, 'month');
+  }
+  return legs;
+};
+
+export {
+  getTradeObject,
+  getTradeIndex,
+  getTotalsReducer,
+  generateLegs,
 };
