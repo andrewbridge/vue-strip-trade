7d5cc0ff59e1ddef1d71230d89bbfae5bcb469f4
Andrew Bridge <me@andrewhbridge.co.uk>
2019-01-14 12:16:15 +0000

    Fill out vuex store

--- src/store.js
+++ src/store.js
@@ -5,12 +5,33 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    trades: [],
+    optionClasses: [
+      'Vanilla',
+      'Cash deals',
+      'Barriers',
+      'European digital',
+      'Partial/Window barrier',
+      'Target structures',
+      'Fader',
+      'Average options',
+      'Accrual options',
+      'Binaries',
+      'Accumulator template',
+    ],
   },
   mutations: {
-
+    addTrade(state, trade) {
+      // push to trades
+    },
+    addStripTrade(state, payload) {
+      // add trade after base trade
+    },
+    editTrade(state, payload) { // edit by path? merge?
+      // Get the trade and merge in changes
+    },
   },
   actions: {
-
+    // actions for the above mutations?
   },
 });
